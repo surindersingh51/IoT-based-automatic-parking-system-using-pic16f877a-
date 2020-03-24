fa480a4ecef70d132f5527156fb29e45dca191de
surindersingh51 <55204398+surindersingh51@users.noreply.github.com>
2020-03-24 00:50:10 -0400

    config header file for stepper motor

---
+++ header_cd.h
@@ -0,0 +1,20 @@
+// PIC16F877A Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG
+#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable bit (BOR enabled)
+#pragma config LVP = OFF        // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+#include <xc.h>
+
+
+#define _XTAL_FREQ 80000000 //Crystal Frequency, used in delay
+#define speed 5 // Speed Range 10 to 1  10 = lowest , 1 = highest
+#define steps 50 // how much step it will take
