33bace10588890be9471457918fcbdb549d52277
surindersingh51 <55204398+surindersingh51@users.noreply.github.com>
2020-04-13 14:25:11 -0400

    functions header file for project code

---
+++ function.h
@@ -0,0 +1,211 @@
+/* 
+ * File:   function.h
+ * Author: SURINDER  MEHTOT
+ *
+ * Created on March 18, 2020, 5:21 PM
+ */
+#include "config.h"
+//LCD pins
+#define RS RB2  //defining pins for LCD interface.
+#define EN RB3
+#define D4 RB4
+#define D5 RB5
+#define D6 RB6
+#define D7 RB7
+//IR sensor PINS
+#define SW1 RD0 //hereby defining pins for IR sensor at entry
+#define SW2 RD1  //IR at first spot
+#define SW3 RD2  //IR at second spot
+#define SW4 RD3  //IR at third spot
+#define SW5 RD4 //IR at fourth spot
+//led pins
+#define LED1 RC5 
+#define LED2 RC6
+#define LED3 RB0
+#define LED4 RB1 
+
+#define redled RC4  //red led at entry
+void Lcd_Port(char a)
+{
+	if(a & 1)
+		D4 = 1;
+	else
+		D4 = 0;
+
+	if(a & 2)
+		D5 = 1;
+	else
+		D5 = 0;
+
+	if(a & 4)
+		D6 = 1;
+	else
+		D6 = 0;
+
+	if(a & 8)
+		D7 = 1;
+	else
+		D7 = 0;
+}
+void Lcd_Cmd(char a)
+{
+	RS = 0;             // => RS = 0
+	Lcd_Port(a);
+	EN  = 1;             // => E = 1
+        __delay_ms(4);
+        EN  = 0;             // => E = 0
+}
+
+void Lcd_Clear()
+{
+	Lcd_Cmd(0);
+	Lcd_Cmd(1);
+}
+
+void Lcd_Set_Cursor(char a, char b)
+{
+	char temp,z,y;
+	if(a == 1)
+	{
+	  temp = 0x80 + b - 1;
+		z = temp>>4;
+		y = temp & 0x0F;
+		Lcd_Cmd(z);
+		Lcd_Cmd(y);
+	}
+	else if(a == 2)
+	{
+		temp = 0xC0 + b - 1;
+		z = temp>>4;
+		y = temp & 0x0F;
+		Lcd_Cmd(z);
+		Lcd_Cmd(y);
+	}
+}
+
+void Lcd_Init()
+{
+  Lcd_Port(0x00);
+   __delay_ms(20);
+  Lcd_Cmd(0x03);
+	__delay_ms(5);
+  Lcd_Cmd(0x03);
+	__delay_ms(11);
+  Lcd_Cmd(0x03);
+  /////////////////////////////////////////////////////
+  Lcd_Cmd(0x02);
+  Lcd_Cmd(0x02);
+  Lcd_Cmd(0x08);
+  Lcd_Cmd(0x00);
+  Lcd_Cmd(0x0C);
+  Lcd_Cmd(0x00);
+  Lcd_Cmd(0x06);
+}
+
+void Lcd_Write_Char(char a)
+{
+   char temp,y;
+   temp = a&0x0F;
+   y = a&0xF0;
+   RS = 1;             
+   Lcd_Port(y>>4);             
+   EN = 1;
+   __delay_us(40);
+   EN = 0;
+   Lcd_Port(temp);
+   EN = 1;
+   __delay_us(40);
+   EN = 0;
+}
+
+void Lcd_Write_String(char *a)
+{
+	int i;
+	for(i=0;a[i]!='\0';i++)
+	   Lcd_Write_Char(a[i]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
